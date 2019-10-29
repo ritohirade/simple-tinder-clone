9af899690e611b14d0b48d2af47bc7cf485e2833
ritohirade <rittonboy89love@gmail.com>
2019-10-29 19:57:59 +0900

    vbcsionekl

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: %i(show index)
+  resources :reactions, only: %i(create)
+  resources :matching, only: %i(index)
 end
