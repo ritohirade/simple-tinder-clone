d910d715479985924505b12be324d6a6aef36976
ritohirade <rittonboy89love@gmail.com>
2019-10-29 21:17:56 +0900

    vbsdoikl

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :chat_messages
+  has_many :chat_room_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +10,8 @@ class User < ApplicationRecord
   mount_uploader :img_name, ImgNameUploader
 
   enum sex: { 男: 0, 女: 1 }
+
+  mount_uploader :img_name, ImgNameUploader
+
+  validates :img_name, presence: true
 end
